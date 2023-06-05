55960a52074067a4252faeb1fac4c6d73c9938d0
Yazan Salameh <yazan@serenbit.ps>
2023-06-05 20:50:11 +0300

    Init build system with SDL libaray

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 
 IF NOT EXIST build mkdir build
 
-clang++ -std=c++17 -g ./src/main.cpp -o ./build/main.exe
+clang++ -std=c++17 -m64 -g ./src/main.cpp -o ./build/main.exe -I./libs/sdl/include -L ./libs/sdl/libs -lSDL2main -lSDL2 -fwritable-strings -Werror -fno-exceptions
